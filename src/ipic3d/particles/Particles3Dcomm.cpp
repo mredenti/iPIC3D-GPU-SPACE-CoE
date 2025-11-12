c5d7bd9b166ef952527e55de19e57bec60849a7e
Michael Redenti <m.redenti@cineca.it>
2025-11-12 14:57:41 +0100

    reserve same capacity

--- src/ipic3d/particles/Particles3Dcomm.cpp
+++ src/ipic3d/particles/Particles3Dcomm.cpp
@@ -1630,7 +1630,7 @@ void Particles3Dcomm::sort_particles_serial_AoS()
 {
   convertParticlesToAoS();
 
-  _pclstmp.reserve(_pcls.size());
+  _pclstmp.reserve(_pcls.capacity());
   {
     numpcls_in_bucket->setall(0);
     // iterate through particles and count where they will go
