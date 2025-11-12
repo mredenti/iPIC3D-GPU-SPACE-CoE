bd36e0f427b67e1827dd27cc040c5bd5761abcba
Michael Redenti <m.redenti@cineca.it>
2025-11-12 14:51:19 +0100

    add capacity info

--- src/ipic3d/main/iPIC3Dlib.cu
+++ src/ipic3d/main/iPIC3Dlib.cu
@@ -241,10 +241,12 @@ int c_Solver::Init(int argc, char **argv) {
 
       // Sort before first device copy
       std::cout << "[DEBUG] species " << i
-                << " before sort N=" << outputPart[i].getNOP() << std::endl;
+                << " before sort size=" << outputPart[i].getNOP()
+                << " cap="  << outputPart[i].get_pcl_array().capacity() << std::endl;
       outputPart[i].sort_particles_serial();
       std::cout << "[DEBUG] species " << i
-                << " after  sort N=" << outputPart[i].getNOP() << std::endl;
+                << " after  sort size=" << outputPart[i].getNOP()
+                << " cap="  << outputPart[i].get_pcl_array().capacity() << std::endl;
     }
   }
 
